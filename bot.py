e3ce0c85b68856770049f5f794129b9c8c144b02
fararipout <fararipout@gmail.com>
2025-07-11 23:48:50 +0330

    Update and rename main (2).py to bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 # راه‌اندازی ربات
 API_ID = '3335796'
 API_HASH = '138b992a0e672e8346d8439c3f42ea78'
-BOT_TOKEN = '5002292255:AAG3EmBHEaTPRxW8hZ797xuES-baLWm29Wo'
+BOT_TOKEN = '5002292255:AAGc9Lk0LXX1cjfERx6CnVye0A5EUNvgtzU'
 
 app = TelegramClient("watermark_bot", api_id=API_ID, api_hash=API_HASH)
 
