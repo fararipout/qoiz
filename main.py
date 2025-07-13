0bc83001bd984f6181619c76510285354de5d4d2
fararipout <fararipout@gmail.com>
2025-07-13 08:39:19 +0330

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ logger = logging.getLogger(__name__)
 # راه‌اندازی ربات
 API_ID = '3335796'
 API_HASH = '138b992a0e672e8346d8439c3f42ea78'
-BOT_TOKEN = '5002292255:AAGc9Lk0LXX1cjfERx6CnVye0A0EUNvgtzU' # Ensure this token is correct
+BOT_TOKEN = '8189638115:AAEYMDvummCXAPgdpavZbYHa3YuXpOzkRBY' # Ensure this token is correct
 
 app = TelegramClient("watermark_bot", api_id=API_ID, api_hash=API_HASH)
 
